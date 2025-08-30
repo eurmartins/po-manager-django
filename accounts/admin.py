53d1880892b5ce65760be333895bac58872a0064
Eurmartins <victormartinssantos.work@gmail.com>
2025-08-30 20:10:43 -0300

    feat: add initial accounts app with user model and admin registration

---
+++ accounts/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models.User import User
+
+#Registrar model
+admin.site.register(User)
---
+++ accounts/models/User.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class User(models.Model):
+    
+    id = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=150, unique=True)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=128)
